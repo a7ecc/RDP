3e2f5d19e5161baa1b6771e53cd7a5b4f7ef066d
a7ecc <102755240+a7ecc@users.noreply.github.com>
2023-02-28 00:39:40 -0800

    Add files via upload

---
+++ Disable.bat
@@ -0,0 +1,5 @@
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f >nul
+netsh advfirewall firewall set rule group="remote desktop" new enable=No >nul
+exit
